6811c35d3556cfbd5b7bbe3d40b8985b131ff4b9
al-amin90 <ijesun30@gmail.com>
2025-12-26 21:42:56 +0600

    feat: Backend installation setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,8 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
 const app = express();
-const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Server is Running on Port .:}');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export default app;
